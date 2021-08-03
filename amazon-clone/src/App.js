b1a543a9dd7740dc6b78cdbf30fdcded50d38a33
am4n-raj <am4nraj@gmail.com>
2021-08-03 17:04:03 +0530

    Add Home Page

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import Home from './Home'
 import './App.css';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div >
       
       <Header />
-      
+      <Home />
       
     </div>
   );
---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="Home">
+            
+            <div className="home_container">
+
+                <img className="home_image" 
+                src='https://m.media-amazon.com/images/G/31/AmazonVideo/2019/1102620_MLP_1440x675_apv189_V3._CB663481141_._CR575,0,865,675_AC_.jpg' alt='Home' />
+            
+
+                <div className="home_row">
+                    {/* {Product} */}
+                    {/* {Product} */}
+                </div>
+
+                <div className="home_row">
+                    {/* {Product} */}
+                    {/* {Product} */}
+                    {/* {Product} */}
+                </div>
+
+                <div className="home_row">
+                    {/* {Product} */}
+                </div>
+            
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
